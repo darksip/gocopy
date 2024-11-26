86732671bd761c8985104827990d11a9496fab58
darksip <pierre.siccardi@gmail.com>
2024-11-26 14:04:20 +0100

    Ajout d'un script launch.bat pour configurer les répertoires source et destination, la liste des fichiers et le nombre de threads pour le processus de copie.

---
+++ launch.bat
@@ -0,0 +1,5 @@
+SET SOURCE_DIR="P:\lossless\"
+SET DEST_DIR="\\192.168.133.230\Sony\Sources Demat\Sony.ddex\lossless\"
+SET FILES_LIST_PATH="sony_special_ludo.csv"
+SET THREAD_COUNT=3
+EXEC .\gocopy.exe
